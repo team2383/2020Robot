68ba637650585e833ae9f2f7c16ee40fac0a2b29
stan173 <stan173@outlook.com>
2020-01-25 17:48:45 -0500

    feeda code finished?
    intake subsystem code and adjusted HAL+OI

--- src/main/java/frc/robot/HAL.java
+++ src/main/java/frc/robot/HAL.java
@@ -8,6 +8,7 @@ public class HAL {
 
     public static Drivetrain drive = new Drivetrain();
     public static Shoota shoota = new Shoota();
+    public static Feeda feeda = new Feeda();
     public static AHRS navX = new AHRS(SPI.Port.kMXP);
     
 }
