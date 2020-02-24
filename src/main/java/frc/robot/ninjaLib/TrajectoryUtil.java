d0c4875329e254a8ec1190da97be0019f051b1dc
John Allswoth <34280998+jallsworth@users.noreply.github.com>
2020-02-24 16:15:13 -0600

    Add files via upload

---
+++ src/main/java/frc/robot/ninjaLib/TrajectoryUtil.java
@@ -0,0 +1,37 @@
+package Util;
+
+public class TrajectoryUtil
+{
+    /*
+    * Parameter distance: units in inches
+    * Parameter ballVelocity: units are inches/per second
+    * Return: the desired hood angle in degrees
+    */
+    public static double getHoodAngle(double distance, double ballVelocity)
+    {
+        final double g = 39.3701;
+        return Math.toDegrees(Math.asin((2*g*distance)/Math.pow(ballVelocity, 2)));
+    }
+
+    /*
+    * Parameter distance: units in inches
+    * Parameter hoodAngle: units in degrees
+    * Return: the desired ball velocity in inches/per second
+    */
+    public static double getDesiredBallVelocity(double distance, double hoodAngle)
+    {
+        final double g = 39.3701;
+        return Math.sqrt((2*g*distance)/(Math.sin(Math.toRadians(2*hoodAngle))));
+    }
+
+    public static double getDesiredMotorOutput(double kP, double desiredBallVelocity)
+    {
+        return kP;
+    }
+
+    public static double getDesiredHoodTicks(double hoodAngle)
+    {
+        final double k = 0.0;
+        return k * hoodAngle;
+    }
+}
