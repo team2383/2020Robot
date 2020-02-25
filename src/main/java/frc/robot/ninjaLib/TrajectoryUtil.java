e0cb204b4ff1cdb0a97289c3bc911971be65c81b
stan173 <stan173@outlook.com>
2020-02-25 11:55:16 -0500

    updated g constant and hood tick converstion

--- src/main/java/frc/robot/ninjaLib/TrajectoryUtil.java
+++ src/main/java/frc/robot/ninjaLib/TrajectoryUtil.java
@@ -9,7 +9,7 @@ public class TrajectoryUtil
     */
     public static double getHoodAngle(double distance, double ballVelocity)
     {
-        final double g = 39.3701;
+        final double g = 39.3701 * 9.8;
         return Math.toDegrees(Math.asin((2*g*distance)/Math.pow(ballVelocity, 2)));
     }
 
@@ -20,16 +20,16 @@ public class TrajectoryUtil
     */
     public static double getDesiredBallVelocity(double distance, double hoodAngle)
     {
-        final double g = 39.3701;
+        final double g = 39.3701 * 9.8;
         return Math.sqrt((2*g*distance)/(Math.sin(Math.toRadians(2*hoodAngle))));
     }
 
     /*
     * Parameter desiredBallVelocity: units in inches/per second
     * Parameter kP: tuning constant to account for air resistence
-    * Return: the desired ball velocity in inches/per second
+    * Return: the desired motor rpm
     */
-    public static double getDesiredMotorOutput(double kP, double desiredBallVelocity)
+    public static double getDesiredMotorRPM(double kP, double desiredBallVelocity)
     {
         final double k = 0.0; 
         return kP*(k*desiredBallVelocity);
@@ -41,7 +41,7 @@ public class TrajectoryUtil
     */
     public static double getDesiredHoodTicks(double hoodAngle)
     {
-        final double k = 0.0;
+        final double k = 109.0;
         return k * hoodAngle;
     }
 }
